0fc0b9512b722b3fc1c412bfdcbbd7f6024a20aa
siljands <siljands@arcada.fi>
2025-10-18 16:14:47 +0300

    Added post endpoint

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ const app = express();
 
 // Create a route that will handle Twilio webhook requests, sent as an
 // HTTP POST to /voice in your application
+app.post('/voice', (request, response) => {
+  // Use the Twilio Node.js SDK to build an XML response
+    const twiml = new VoiceResponse();
+
+    const facts = ["Fun fact 1", "Fun fact 2", "Fun fact 3", "Fun fact 4"];
+    let i = Math.floor(Math.random() * facts.length);
+    let r = facts[i];
+
+    twiml.say(r);
+
+  // Render the response as XML in reply to the webhook request
+  response.type('text/xml');
+  response.send(twiml.toString());
+});
+
+// HTTP GET to /voice for testing
 app.get('/voice', (request, response) => {
   // Use the Twilio Node.js SDK to build an XML response
     const twiml = new VoiceResponse();
