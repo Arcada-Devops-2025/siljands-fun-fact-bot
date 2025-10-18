b06fa74f55bf57647d72d92398586762a9ad115c
siljands <siljands@arcada.fi>
2025-10-18 10:32:06 +0300

    Added first version of the bot

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # siljands-fun-fact-bot
-A Twilio bot that replies with fun facts
+A Twilio bot that replies with fun facts.
+
+# How to run the bot
+
+1. Clone github project
+2. Install express: https://expressjs.com/
+3. Install the Twilio Node.js SDK: https://github.com/twilio/twilio-node
+4. Start server (node server.js)
+5. Go to http://127.0.0.1:3000/ and get your fact
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+
+const app = express();
+
+// Create a route that will handle Twilio webhook requests, sent as an
+// HTTP POST to /voice in your application
+app.get('/voice', (request, response) => {
+  // Use the Twilio Node.js SDK to build an XML response
+    const twiml = new VoiceResponse();
+
+    const facts = ["Fun fact 1", "Fun fact 2", "Fun fact 3", "Fun fact 4"];
+    let i = Math.floor(Math.random() * facts.length);
+    let r = facts[i];
+
+    twiml.say(r);
+
+  // Render the response as XML in reply to the webhook request
+  response.type('text/xml');
+  response.send(twiml.toString());
+});
+
+// Create an HTTP server and listen for requests on port 3000
+app.listen(3000, () => {
+    console.log('TwiML server running at http://127.0.0.1:3000/');
+});
